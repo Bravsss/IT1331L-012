cce6524d0a9bd79a10b6e15b641ffec03beccc52
ROG Bravs <christian.bravo@uap.asia>
2023-11-06 15:55:15 +0800

    no message

---
+++ password-encryptor-api/src/main/java/com/uap/it1311l/passwordencryptorapi/models/EncryptionResponse.java
@@ -0,0 +1,30 @@
+package com.uap.it1311l.passwordencryptorapi.models;
+
+public class EncryptionResponse {
+    private String status;
+    private String result;
+
+    public EncryptionResponse() {
+    }
+
+    public EncryptionResponse(String status, String result) {
+        this.status = status;
+        this.result = result;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getResult() {
+        return result;
+    }
+
+    public void setResult(String result) {
+        this.result = result;
+    }
+}
